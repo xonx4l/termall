ddde82f67d8e805fa7ce80e86576bb3b1bb7cfc7
xonx4l <sk797119@gmail.com>
2025-05-22 23:27:52 +0530

    change fd to File

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 use eframe::egui;
+use::nix::unistd::ForkResult;
+use std::ffi::{Cstr, Cstring};
 
 fn main() {
-    unsafe{
+      let fd = unsafe{
           let res = nix::pty::forkpty(None, None);
+          match res.fork.result{
+            ForkResult::Parent {..} => (),
+            ForkResult::Child => {
+                let shell_name = Cstr::from_bytes_with_null(b"ash\0").expect("Should always have null terminator");
+                nix::unistd::execvp::<Cstring>(shell_name, &[]).unwrap();
+                return 
+            }
+          }
+          res.master
     }
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My Terminal", native_options, Box::new(|cc| Ok(Box::new(Termali::new(cc)))));
+    eframe::run_native("My Terminal", native_options, Box::new( move |cc| Ok(Box::new(Termali::new(cc, fd)))));
 }
 
 #[derive(Default)]
-struct Termali {}
+struct Termali {
+    buf:String,
+    fd: OwnedFd,
+}
 
 impl Termali {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        Self::default()
+        Termali {
+            buf: String::new(),
+            fd,
+        }
     }
 }
 
