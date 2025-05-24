974895012f7b4bce3035e87a404f27a6bc7ebacd
xonx4l <sk797119@gmail.com>
2025-05-24 18:18:20 +0530

    removing bugs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use eframe::egui;
-use::nix::unistd::ForkResult;
+use nix::unistd::ForkResult;
 use std::{io::Read, fs::File, ffi::{CStr, CString}, os::fd::OwnedFd};
 
 fn main() {
-      let fd = unsafe{
+      let fd = unsafe {
           let res = nix::pty::forkpty(None, None).unwrap();
           match res.fork_result {
             ForkResult::Parent { .. } => (),
@@ -11,7 +11,7 @@ fn main() {
                 let shell_name = CStr::from_bytes_with_nul(b"ash\0").expect("Should always have null terminator");
                 nix::unistd::execvp::<CString>(shell_name, &[]).unwrap();
                 return 
-            }
+             }
           }
           res.master
     };
@@ -21,7 +21,7 @@ fn main() {
     eframe::run_native("My Terminal", native_options, Box::new( move |cc| Box::new(Termali::new(cc, fd))));
 }
 
-#[derive(Default)]
+
 struct Termali {
     buf:Vec<u8>,
     fd: File,
@@ -35,27 +35,27 @@ impl Termali {
         // for e.g. egui::PaintCallback.
         Termali {
             buf: Vec::new(),
-            fd:fd.into(),
+            fd: fd.into(),
         }
     }
 }
 
 impl eframe::App for Termali {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       let mut buf = vec![0u8;4096];
+       let mut buf = vec![0u8; 4096];
        match self.fd.read(&mut buf){
         Ok(read_size) => {
-        self.buf.extend_from_slice(&buf[0..read_size]);
+             self.buf.extend_from_slice(&buf[0..read_size]);
         }
         Err(e) => {
-            println!("failed to read: {e}");
-        }
+            println!("Failed to read: {e}");
+          }
 
        }                  
        egui::CentralPanel::default().show(ctx, |ui| {
-        unsafe{
+        unsafe {
            ui.label(std::str::from_utf8_unchecked(&self.buf))
-        }
+          }
        });
    }
 }
