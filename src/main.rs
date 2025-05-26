981265c0bc2a40c5b4a128762166a47fd0960f5b
xonx4l <sk797119@gmail.com>
2025-05-26 11:11:58 +0530

    fixing code and bugs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     io::Read,
     fs::File,
     ffi::{CStr, CString},
-    os::fd::{OwnedFd, AsRawFd}, 
+    os::fd::OwnedFd, 
 };
 
 fn main() {
@@ -11,7 +11,7 @@ fn main() {
         let res = nix::pty::forkpty(None, None).unwrap();
 
         match res {
-            nix::pty::ForkptyResult::Parent { child: _, master } => { 
+            nix::pty::ForkptyResult::Parent { child: _, master } => {
                 master
             }
             nix::pty::ForkptyResult::Child => {
@@ -31,7 +31,7 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "My Terminal",
+        " My Termall",
         native_options,
         Box::new(move |cc| Ok(Box::new(Termali::new(cc, fd)))),
     );
@@ -41,14 +41,24 @@ fn main() {
 
 struct Termali {
     buf: Vec<u8>,
+    cursor_pos: (usize, usize),
     fd: File,
 }
 
 impl Termali {
     fn new(_cc: &eframe::CreationContext<'_>, fd: OwnedFd) -> Self {
+        
+        let flags = nix::fcntl::fcntl(&fd, nix::fcntl::FcntlArg::F_GETFL).unwrap();
+        let mut flags = nix::fcntl::OFlag::from_bits(flags & nix::fcntl::OFlag::O_ACCMODE.bits()).unwrap();
+        flags.set(nix::fcntl::OFlag::O_NONBLOCK, true);
+
+        
+        nix::fcntl::fcntl(&fd, nix::fcntl::FcntlArg::F_SETFL(flags)).unwrap();
+
         Termali {
             buf: Vec::new(),
-            fd: fd.into(), // Convert OwnedFd into a File
+            fd: fd.into(), 
+            cursor_pos: (0, 0),
         }
     }
 }
@@ -56,47 +66,41 @@ impl Termali {
 // ---
 
 impl eframe::App for Termali {
-   fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-       let mut buf = vec![0u8; 4096];
-       match self.fd.read(&mut buf){
-        Ok(read_size) => {
-             self.buf.extend_from_slice(&buf[0..read_size]);
-        }
-        Err(e) => {
-            // Check for EIO (Input/output error) which can happen when the PTY closes.
-            // Other errors like EAGAIN (Resource temporarily unavailable) might also occur
-            // if the read is non-blocking and no data is ready.
-            if e.kind() != std::io::ErrorKind::WouldBlock { // Only print if not a "would block" error
-                eprintln!("Failed to read from PTY: {e}");
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut buf = vec![0u8; 4096];
+        match self.fd.read(&mut buf) {
+            Ok(read_size) => {
+                self.buf.extend_from_slice(&buf[0..read_size]);
             }
-          }
-
-       }
-       egui::CentralPanel::default().show(ctx, |ui| {
-        ui.input(|input_state| {
-            for event in &input_state.events {
-                let text = match event {
-                    egui::Event::Text(text) => text,
-                    egui::Event::Key { key : egui::Key::Enter,  ..} => {
-                        "/n"
-                    }
-                    _ => "",
-                };
-
-                let bytes = text.as_bytes();
-                let mut to_write: &[u8] = bytes;
-
-                while to_write.len() > 0 {
-                    
-                    let written = nix::unistd::write(&self.fd, to_write).unwrap(); 
-                    
-                    to_write = &to_write[written..];
+            Err(e) => {
+                if e.kind() != std::io::ErrorKind::WouldBlock {
+                    eprintln!("Failed to read from PTY: {e}");
                 }
             }
+        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.input(|input_state| {
+                for event in &input_state.events {
+                    let text = match event {
+                        egui::Event::Text(text) => text,
+                        egui::Event::Key { key : egui::Key::Enter, pressed: true,  ..} => {
+                            "\n"
+                        }
+                        _ => "",
+                    };
+
+                    let bytes = text.as_bytes();
+                    let mut to_write: &[u8] = bytes;
+
+                    while to_write.len() > 0 {
+                        let written = nix::unistd::write(&self.fd, to_write).unwrap();
+                        to_write = &to_write[written..];
+                    }
+                }
+            });
+            unsafe {
+                ui.label(std::str::from_utf8_unchecked(&self.buf))
+            }
         });
-        unsafe {
-           ui.label(std::str::from_utf8_unchecked(&self.buf))
-          }
-       });
-   }
+    }
 }
